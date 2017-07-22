d7b6936a444103f56baa930de721588c81839b8d
CDPatel13 <cdpatel1991@gmail.com>
2017-07-22 22:59:35 +0530

    Updated for state and code related bug

--- GST_Billing/BaseModel.cs
+++ GST_Billing/BaseModel.cs
@@ -26,16 +26,39 @@ namespace GST_Billing
             }
         }
 
-        public Dictionary<string, string> stateCodes = new Dictionary<string, string>(){{"Andaman and Nicobar Islands","35"}, {"Andhra Pradesh","28"},/* {"Andhra Pradesh(New)","37"},*/ 
-                                                    {"Arunachal Pradesh","12"},
-                                                    {"Assam","18"}, {"Bihar","10"}, {"Chandigarh","04"}, {"Chattisgarh","22"}, {"Dadra and Nagar Haveli","26"},
-                                                    {"Daman and Diu","25"}, {"Delhi","07"}, {"Goa","30"}, {"Gujarat","24"}, {"Haryana","06"}, {"Himachal Pradesh","02"},
-                                                    {"Jammu and Kashmir","01"}, {"Jharkhand","20"}, {"Karnataka","29"}, {"Kerala","32"}, {"Lakshadweep Islands","31"},
-                                                    {"Madhya Pradesh","23"}, {"Maharashtra","27"}, {"Manipur","14"}, {"Meghalaya","17"}, {"Mizoram","15"}, {"Nagaland","13"},
-                                                    {"Odisha","21"}, {"Pondicherry","34"}, {"Punjab","03"}, {"Rajasthan","08"}, {"Sikkim","11"}, {"Tamil Nadu","33"},
-                                                    {"Telangana","36"}, {"Tripura","16"}, {"Uttar Pradesh","09"}, {"Uttarakhand","05"}, {"West Bengal","19"}};
+        public Dictionary<string, string> stateCodes = new Dictionary<string, string>(){{"Andaman And Nicobar Islands","35"}, {"Andhra Pradesh","28"},/* {"Andhra Pradesh(New)","37"},*/ 
+                                                    {"Arunachal Pradesh","12"}, {"Assam","18"}, {"Bihar","10"}, {"Chandigarh","04"}, {"Chattisgarh","22"},
+                                                    {"Dadra And Nagar Haveli","26"}, {"Daman And Diu","25"}, {"Delhi","07"}, {"Goa","30"}, {"Gujarat","24"},
+                                                    {"Haryana","06"}, {"Himachal Pradesh","02"},{"Jammu And Kashmir","01"}, {"Jharkhand","20"}, {"Karnataka","29"},
+                                                    {"Kerala","32"}, {"Lakshadweep Islands","31"}, {"Madhya Pradesh","23"}, {"Maharashtra","27"}, {"Manipur","14"},
+                                                    {"Meghalaya","17"}, {"Mizoram","15"}, {"Nagaland","13"}, {"Odisha","21"}, {"Pondicherry","34"}, {"Punjab","03"},
+                                                    {"Rajasthan","08"}, {"Sikkim","11"}, {"Tamil Nadu","33"}, {"Telangana","36"}, {"Tripura","16"}, {"Uttar Pradesh","09"},
+                                                    {"Uttarakhand","05"}, {"West Bengal","19"}};
 
-        
+
+        // Convert the string to Pascal case.
+        public string ToPascalCase(string the_string)
+        {
+            // If there are 0 or 1 characters, just return the string.
+            if (the_string == null) return the_string;
+            if (the_string.Length < 2) return the_string.ToUpper();
+
+            // Split the string into words.
+            string[] words = the_string.Split(
+                new char[] { },
+                StringSplitOptions.RemoveEmptyEntries);
+
+            // Combine the words.
+            string result = "";
+            foreach (string word in words)
+            {
+                result +=
+                    word.Substring(0, 1).ToUpper() +
+                    word.Substring(1) + " ";
+            }
+
+            return result.Trim();
+        }
 
         public float Quantity
         {
--- GST_Billing/Invoice.cs
+++ GST_Billing/Invoice.cs
@@ -456,7 +456,8 @@ namespace GST_Billing
 
         private void cbShipState_SelectedIndexChanged(object sender, EventArgs e)
         {
-            tbShipCode.Text = baseModel.stateCodes[cbShipState.Text];
+            string state = baseModel.ToPascalCase(cbShipState.Text);
+            tbShipCode.Text = baseModel.stateCodes[state];
         }
 
         private void textBox_TextChanged(object sender, EventArgs e)
